46e814cf7dce5b26347573e6a7c7507b635a17f9
A A Otoo <alexotoous@outlook.com>
2020-09-06 18:31:33 +0000

    first upload
    An imaginary luxury watch website built experimenting with Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        cblack: "#0C0B0B",
+        cwhite: "#F7F7F7",
+        cgray: {
+          100: "#A5A4A3",
+          200: "#5C5B5B",
+        },
+        cbrown: {
+          100: "#CAB7A7",
+          200: "#AF8F75",
+          300: "#8E5E3B",
+          500: "#583824",
+        },
+      },
+      fontFamily: {
+        hero: ["Great Vibes"],
+        body: ["Raleway"],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+  "tailwindCSS.includeLanguages": {
+    plaintext: "html",
+  },
+};
